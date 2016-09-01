0511d310d11158c44527101b27a016f4a3cf6a6c
SeoDaeHoon <gomme9396@gmail.com>
2016-09-01 17:39:03 +0900

    컨트롤러 추가

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,2 +1,16 @@
 class HomeController < ApplicationController
+  def index
+  end
+
+  def write_customer
+    @name_customer = params[:name_customer];
+    @email_customer = params[:email_customer];
+  end
+
+  def write_artist
+    @name_artist = params[:name_artist];
+    @phone_artist = params[:phone_artist];
+    @email_artist = params[:email_artist];
+  end
+  
 end
