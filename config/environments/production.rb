b51cd310a2cc09ff13ff135e38745521c9e146e8
SeoDaeHoon <gomme9396@gmail.com>
2017-04-15 14:45:43 +0900

    mail api 수정

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,7 +81,7 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :mailgun
   config.action_mailer.mailgun_settings = {
-		api_key: 'ENV['MAILGUN_API_KEY']',
-		domain: 'ENV['DOMAIN']',
+		api_key: ENV['MAILGUN_API_KEY'],
+		domain: ENV['DOMAIN']
   }
 end
